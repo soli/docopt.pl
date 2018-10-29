26ee0fb5a3553bb7ddfe10dedb01f9cffd136c22
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2018-10-29 14:23:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an implementation of [Docopt](http://docopt.org/) for SWI-Prolog
---
+++ pack.pl
@@ -0,0 +1,4 @@
+name(docopt).
+title('docopt.org implementation').
+version('0.1').
+author('Sylvain Soliman', 'Sylvain.Soliman@inria.fr').
